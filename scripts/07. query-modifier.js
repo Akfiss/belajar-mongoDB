bc69ed98e7a829c8d0fbdbc2071e3979c7a1a0cc
Akbar Firdaus <101693895+Akfiss@users.noreply.github.com>
2022-06-05 14:37:19 +0800

    Update 07. query-modifier.js

--- scripts/07. query-modifier.js
+++ scripts/07. query-modifier.js
@@ -1,17 +1,17 @@
-// select count(*) from products
-db.products.find({}).count()
+// select count(*) from jenis_barang
+db.jenis_barang.find({}).count()
 
-// select * from products limit 4
-db.products.find({}).limit(4)
+// select * from jenis_barang limit 3
+db.jenis_barang.find({}).limit(3)
 
-// select * from products offset 2
-db.products.find({}).skip(2)
+// select * from customers offset 2
+db.customers.find({}).skip(2)
 
-// select * from products limit 4 offset 2
-db.products.find({}).limit(4).skip(2)
+// select * from customers limit 3 offset 2
+db.customers.find({}).limit(3).skip(2)
 
-// select * from products order by name asc, category desc
-db.products.find({}).sort({
-    name: 1,
-    category: -1
+// select * from customers order by nama asc, kategori desc
+db.customers.find({}).sort({
+    nama: 1,
+    kategori: -1
 })
