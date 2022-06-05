ae442b06753bf80df051379724761116779af01f
Akbar Firdaus <101693895+Akfiss@users.noreply.github.com>
2022-06-05 14:43:57 +0800

    Update 07. query-modifier.js

--- scripts/07. query-modifier.js
+++ scripts/07. query-modifier.js
@@ -10,8 +10,8 @@ db.customers.find({}).skip(2)
 // select * from customers limit 3 offset 2
 db.customers.find({}).limit(3).skip(2)
 
-// select * from customers order by nama asc, kategori desc
-db.customers.find({}).sort({
+// select * from jenis_barang order by nama asc, kategori desc
+db.jenis_barang.find({}).sort({
     nama: 1,
     kategori: -1
 })
